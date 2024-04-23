b0d31f6fdeee81499b5670734de7ce620c01b7ee
Dilanka Liyanagama <prasadliyanagamad@gmail.com>
2024-04-23 17:27:57 +0530

    Implement spring security

--- src/main/java/com/fitness_social_media/fitness_social_media/controller/UserController.java
+++ src/main/java/com/fitness_social_media/fitness_social_media/controller/UserController.java
@@ -35,33 +35,33 @@ public class UserController {
     }
 
     // Get all users
-    @GetMapping("/users")
+    @GetMapping("/api/users")
     public List<User> getUsers() {
         List<User> users = userRepository.findAll();        
         return users;
     }
 
     // Get a specific user by id
-    @GetMapping("/users/{userId}")
+    @GetMapping("/api/users/{userId}")
     public User getUserById(@PathVariable ("userId") Integer id) throws Exception {        
         User user = userService.findUserById(id);
         return user;
     }
     
     // Update an existing user by Id
-    @PutMapping("/users/{userId}")
+    @PutMapping("/api/users/{userId}")
     public User updateUser(@RequestBody User user, @PathVariable ("userId") Integer id) throws Exception{
        User updatedUser=userService.updateUser(user, id);
        return updatedUser;
     }
 
-    @PutMapping("/users/follow/{userId1}/{userId2}")
+    @PutMapping("/api/users/follow/{userId1}/{userId2}")
     public User followUserHandler(@PathVariable Integer userId1,@PathVariable Integer userId2) throws Exception{
         User user=userService.followUser(userId1, userId2);
         return user;
     }
 
-    @GetMapping("/users/search")
+    @GetMapping("/api/users/search")
     public List<User> searchUser(@RequestParam("query") String query){
         List<User> users=userService.searchUser(query);
         return users;
